e38b69db3359de59e980877ac296ad414f69bb59
conradsp <sconrad@digitalimpactalliance.org>
2021-03-15 15:09:54 -0500

    Set up infinite scroll for products

--- components/products/ProductCard.js
+++ components/products/ProductCard.js
@@ -3,7 +3,7 @@ import ReactHtmlParser from 'react-html-parser'
 const ProductCard = ({ product }) => (
   <div className='max-w-sm bg-white border-2 border-gray-300 p-6 rounded-md tracking-wide shadow-lg'>
     <div id='header' className='flex items-center mb-4'> 
-      <img alt='avatar' className='w-20 rounded-full border-2 border-gray-300' src={'http://localhost:3000/' + product.imageFile} />
+      <img alt='avatar' className='w-20 rounded-full border-2 border-gray-300' src={process.env.NEXT_PUBLIC_GRAPHQL_SERVER + product.imageFile} />
       <div id='header-text' className='leading-5 ml-6 sm'>
         <h4 id='name' className='text-xl font-semibold'>{product.name}</h4>
       </div>
--- components/products/ProductList.js
+++ components/products/ProductList.js
@@ -1,16 +1,28 @@
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
 import ProductCard from './ProductCard'
 
+const DEFAULT_PAGE_SIZE = 20
+
 const PRODUCTS_QUERY = gql`
-query {
-  products {
-    id
-    name
-    slug
-    imageFile
-    productDescriptions {
-      description
+query Products($first: Int, $after: String) {
+  products(first: $first, after: $after) {
+    pageInfo {
+      endCursor
+      startCursor
+      hasPreviousPage
+      hasNextPage
+    }
+    nodes {
+      id
+      name
+      slug
+      imageFile
+      productDescriptions {
+        description
+      }
     }
   }
 }
@@ -19,27 +31,56 @@ query {
 const ProductList = (props) => {
   return (
     <>
-      <div id='content' className='container-fluid with-header'>
-        <div className='row flex flex-wrap grid grid-cols-4 gap-3'>
-          {props.productList.map((product) => {
-            return <ProductCard key={product.id} product={product} />
-          })}
-        </div>
+      <div className='row flex flex-wrap grid grid-cols-4 gap-3'>
+        {props.productList.map((product) => {
+          return <ProductCard key={product.id} product={product} />
+        })}
       </div>
     </>
   )
 }
 
 const ProductListQuery = () => {
-  const { loading, error, data } = useQuery(PRODUCTS_QUERY)
+  const { loading, error, data, fetchMore } = useQuery(PRODUCTS_QUERY, { variables: { first: 20 } })
   if (loading) {
     return <div>Fetching..</div>
   }
   if (error) {
     return <div>Error!</div>
   }
+
+  const { products: { nodes, pageInfo } } = data
+
+  function handleLoadMore () {
+    fetchMore({
+      variables: {
+        after: pageInfo.endCursor,
+        first: DEFAULT_PAGE_SIZE
+      },
+      updateQuery (previousResult, { fetchMoreResult }) {
+        const connection = fetchMoreResult.products
+
+        return {
+          products: {
+            pageInfo: connection.pageInfo,
+            nodes: [...previousResult.products.nodes, ...connection.nodes],
+            __typename: previousResult.products.__typename
+          }
+        }
+      }
+    })
+  }
   return (
-    <ProductList productList={data.products} />
+    <InfiniteScroll
+      dataLength={nodes.length}
+      next={handleLoadMore}
+      hasMore={pageInfo.hasNextPage}
+      loader={<div>Loading...</div>}
+    >
+      <div id='content' className='container-fluid with-header' >
+        <ProductList productList={nodes} />
+      </div>
+    </InfiniteScroll>
   )
 }
 
