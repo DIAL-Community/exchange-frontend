7df2731c720090d08ae645f11f4d649627696740
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-03-17 21:58:08 +0000

    Authentication

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,27 @@
-const Header = () => (
-  <nav className='flex items-center justify-between flex-wrap bg-white p-6 w-full border-solid border-b-2 border-dialyellow'>
-    <span className='font-semibold text-xl tracking-tight text-dialgray-darkest float-left'>Catalog of Digital Solutions</span>
-    <div className='w-full block lg:w-auto float-right'>
-      <div className='float-right'>Menu 1</div>
-    </div>
-  </nav>
-)
+import { signIn, signOut, useSession } from 'next-auth/client'
+
+const Header = () => {
+  const [session] = useSession()
+
+  return (
+    <nav className='flex items-center justify-between flex-wrap bg-white p-6 w-full border-solid border-b-2 border-dialyellow'>
+      <span className='font-semibold text-xl tracking-tight text-dialgray-darkest float-left'>Catalog of Digital Solutions</span>
+      <div className='w-full block lg:w-auto float-right'>
+        <div className='float-right'>
+          {!session &&
+            <>
+              Not signed in <br />
+              <button onClick={() => signIn()}>Sign in</button>
+            </>}
+          {session &&
+            <>
+              Signed in as {session.user.email} <br />
+              <button onClick={() => signOut()}>Sign out</button>
+            </>}
+        </div>
+      </div>
+    </nav>
+  )
+}
 
 export default Header
---
+++ pages/auth/error.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+import Header from '../../components/Header'
+
+export default function Error ({ csrfToken }) {
+  return (
+    <>
+      <Header />
+      <div id='content' className='container-fluid with-header'>
+        <h1 className='block'>Error in authentication</h1>
+        <div className='flex items-center'>
+          <Link href='/auth/signin'>
+            <button className='bg-dialgray-dark hover:bg-blue-dark text-dialgray-light font-bold py-2 px-4 mx-2 rounded'>
+              Try Again
+            </button>
+          </Link>
+          <Link href='/'>
+            <button className='bg-dialgray-dark hover:bg-blue-dark text-dialgray-light font-bold py-2 px-4 mx-2 rounded'>
+              Go Back to Catalog
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
