1079992321a03157255a51ff5da6470dab5f9853
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-03-23 18:36:06 +0000

    Add Menu items to header

--- translations/fr.js
+++ translations/fr.js
@@ -1,6 +1,13 @@
 export const fr = {
   'header.signIn': 'Sign In (FR)',
   'header.signOut': 'Sign Out (FR)',
+  'header.covidResources': 'COVID-19 Resources',
+  'header.help': 'Help',
+  'header.about': 'About',
+  'header.selectLanguage': 'Select Language',
+  'header.english': 'English (en)',
+  'header.german': 'Deutsch (de)',
+  'header.french': 'Francais (fr)',
 
   'products.header': 'Products (FR)'
 }
