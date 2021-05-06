825436673004f78c4be4c57ff1e28e4721c47fec
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-05-06 20:16:17 +0000

    Discourse Integration

---
+++ pages/api/discourse.js
@@ -0,0 +1,41 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+  console.log(req.query)
+  const apiKey = process.env.NEXT_PUBLIC_DISCOURSE_KEY
+  const url = 'https://discourse.govstack.global/'
+  
+  const userResponse = fetch(url+'u/'+req.query.username, {
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Api-Username',
+      'Api-Key': apiKey,
+      'Api-Username': 'system'
+    }
+  }).then(userRes => {
+    if (userRes.status == 404) {
+      return res.status(404).json({})
+    }
+    const postData = {
+      'topic_id': req.query.topicId,
+      'raw': req.query.raw
+    }
+    const response = fetch(url+'posts.json', {
+      method: 'POST', 
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Headers': 'Api-Username',
+        'Api-Key': apiKey,
+        'Api-Username': req.query.username
+      },
+        body: JSON.stringify(postData) 
+      }).then(response => response.json()
+        .then(data => {
+          return res.status(response.status).json(data)
+        })
+      )
+    }
+  )
+}
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -11,13 +11,13 @@ export default function SignIn ({ csrfToken }) {
           <input name='csrfToken' type='hidden' defaultValue={csrfToken} />
           <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col'>
             <div className='mb-4'>
-              <label className='block text-grey-darker text-sm font-bold mb-2' for='username'>
+              <label className='block text-grey-darker text-sm font-bold mb-2' htmlFor='username'>
                 Username
               </label>
               <input className='shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker' id='username' name='username' type='text' placeholder='Username' />
             </div>
             <div className='mb-6'>
-              <label className='block text-grey-darker text-sm font-bold mb-2' for='password'>
+              <label className='block text-grey-darker text-sm font-bold mb-2' htmlFor='password'>
                 Password
               </label>
               <input className='shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3' id='password' name='password' type='password' placeholder='******************' />
