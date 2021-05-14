61dc92d56d39be5d50f2b53d9d12443a2fd82511
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-05-13 20:21:28 -0500

    Updates to rails auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth'
+import { getCsrfToken } from 'next-auth/client'
 import Providers from 'next-auth/providers'
 
 export default NextAuth({
@@ -15,19 +16,39 @@ export default NextAuth({
         password: {  label: "Password", type: "password" }
       },
       async authorize(credentials) {
+        
+        const res = await fetch(process.env.NEXT_PUBLIC_AUTH_SERVER + '/authenticate/token', {
+          mode: 'cors',
+          credentials: 'include',
+          headers: {
+            'Access-Control-Allow-Origin': 'http://test.local.com:3000',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Headers': 'set-cookie'
+          }
+        })
+        const token = await getCsrfToken()//res.json()
+        console.log(token)
+        
         const response = await fetch(process.env.NEXT_PUBLIC_AUTH_SERVER + '/authenticate/credentials', {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
+            //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'include', // include, *same-origin, omit
             headers: {
-              'Content-Type': 'application/json'
+              'Content-Type': 'application/json',
+              'X-Requested-With': 'XMLHttpRequest',
+              'Access-Control-Allow-Origin': 'http://127.0.0.1:3000',
+              'Access-Control-Allow-Credentials': true,
+              'Access-Control-Allow-Headers': 'set-cookie',
+              //'X-CSRF-Token': token //document.querySelector('meta[name="csrf-token"]').attr('content')
             },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            //redirect: 'follow', // manual, *follow, error
+            //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
             body: JSON.stringify(credentials) // body data type must match "Content-Type" header
           }) 
-        const user = response.json();
+        const user = await response.json()
+        console.log(response.headers)
+        console.log(response.headers.get('set-cookie'))
         if (user) {
           // Any user object returned here will be saved in the JSON Web Token
           return user
