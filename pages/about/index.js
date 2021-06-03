a0f5f34699b756a5ec5717168f93243c16f50e6c
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2021-06-03 12:57:10 -0400

    Adding header information to the about page.

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,10 +2,18 @@ import Header from '../../components/Header'
 import Definition from '../../components/Definition'
 import Footer from '../../components/Footer'
 
-const AboutPage = () => {
+import Head from 'next/head'
+import { useIntl } from 'react-intl'
 
+const AboutPage = () => {
+  const { formatMessage } = useIntl()
+  const format = (id) => formatMessage({ id })
   return (
     <>
+      <Head>
+        <title>{format('app.title')}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <Header />
       <Definition />
       <Footer />
