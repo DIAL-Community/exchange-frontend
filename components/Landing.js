1ec85fb7ef04eb3fc766480db0c40ebecc20db80
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-07-06 18:25:06 -0500

    FIx for import

--- components/Landing.js
+++ components/Landing.js
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import { useIntl } from 'react-intl'
 
-import Link from 'next/link'
-
 const Landing = () => {
   const { formatMessage } = useIntl()
   const format = (id) => formatMessage({ id })
