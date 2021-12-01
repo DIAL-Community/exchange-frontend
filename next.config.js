52b0ba2a9fa5fbdff9d3a611c2f42ee66fd40eb2
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-12-01 10:51:57 -0600

    Testing CDN configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  assetPrefix: isProd ? 'https://cdn.solutions.dial.community' : '',
   i18n: {
     locales: ['en', 'fr', 'de', 'cs'],
     defaultLocale: 'en',
