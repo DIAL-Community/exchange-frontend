737956ce2a7f30477730c11f9989d45b82cbd9d3
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-12-01 14:04:01 -0600

    Update CDN URL

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? 'https://cdn.solutions.dial.community' : '',
+  assetPrefix: isProd ? 'https://cdn.solutions-dev.dial.community' : '',
   i18n: {
     locales: ['en', 'fr', 'de', 'cs'],
     defaultLocale: 'en',
