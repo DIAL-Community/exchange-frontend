390a58a53ec1c6ed52cb08454c843a5901d0482a
Steve Conrad <sconrad@digitalimpactalliance.org>
2021-12-15 15:18:17 +0000

    New front end prompts for pt, es, sw

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
-    locales: ['en', 'fr', 'de', 'cs'],
+    locales: ['en', 'fr', 'de', 'cs', 'es', 'pt', 'sw'],
     defaultLocale: 'en',
     localeDetection: false
   }
