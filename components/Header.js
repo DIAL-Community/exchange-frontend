e8740a144a8ca78b78748f512084bed891f99a4c
Steve Conrad <sconrad@digitalimpactalliance.org>
2022-05-02 14:35:20 +0000

    CDS-279: Fix Candidate roles flow and add admin screens

--- components/Header.js
+++ components/Header.js
@@ -83,6 +83,9 @@ const AdminMenu = () => {
           <a href='/candidate/products' role='menuitem' className={dropdwonMenuStyles}>
             {format('header.admin.candidate_products')}
           </a>
+          <a href='/candidate/roles' role='menuitem' className={dropdwonMenuStyles}>
+            {format('header.admin.candidate_roles')}
+          </a>
           <a href={`${process.env.NEXT_PUBLIC_RAILS_SERVER}/deploys?user_email=${userEmail}&user_token=${userToken}`} role='menuitem' className={dropdwonMenuStyles}>
             {format('header.admin.deploys')}
           </a>
