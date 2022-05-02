53dec48b1c6e54a8fc5d7db8f463509fd0594c74
Steve Conrad <sconrad@digitalimpactalliance.org>
2022-05-02 15:21:36 -0500

    Add Country edit to admin menu

--- components/Header.js
+++ components/Header.js
@@ -74,6 +74,9 @@ const AdminMenu = () => {
           <a href={`${process.env.NEXT_PUBLIC_RAILS_SERVER}/sectors?user_email=${userEmail}&user_token=${userToken}`} role='menuitem' className={dropdwonMenuStyles}>
             {format('header.admin.sectors')}
           </a>
+          <a href={`${process.env.NEXT_PUBLIC_RAILS_SERVER}/countries?user_email=${userEmail}&user_token=${userToken}`} role='menuitem' className={dropdwonMenuStyles}>
+            {format('header.admin.countries')}
+          </a>
           <a href={`${process.env.NEXT_PUBLIC_RAILS_SERVER}/tags?user_email=${userEmail}&user_token=${userToken}`} role='menuitem' className={dropdwonMenuStyles}>
             {format('header.admin.tags')}
           </a>
