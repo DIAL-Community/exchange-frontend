43acb59abc2b5e238bc12cf9a57e2f200c628d28
Steve Conrad <sconrad@digitalimpactalliance.org>
2022-05-17 16:28:41 -0500

    Add image and update homepage

--- pages/_sites/govstack/index.js
+++ pages/_sites/govstack/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useIntl } from 'react-intl'
-import { useContext } from 'react'
+import Image from 'next/image'
 import dynamic from 'next/dynamic'
 import Header from './components/Header'
 import Landing from './components/Landing'
@@ -20,7 +20,17 @@ const HomePage = () => {
       <Header />
       <Landing />
       <ReactTooltip className='tooltip-prose bg-dial-gray-dark text-white rounded' />
-      
+      <div className='p-24'>
+        <div className='inline-block p-8 bg-blue-900 text-3xl text-bold text-white'>Context and Challenge</div>
+        <div className='w-full py-4 pl-16 flex'>
+          <Image src='/images/govstack/govstack1.webp' alt='Govstack Image' width={500} height={300} />
+          <div className='ml-5 w-1/3 pt-8'>
+            Digital government services are vital for fostering economic growth, developing the digital economy and promoting trust in government institutions.  
+            <br /><br />
+            Governments struggle to keep pace with the digitalization trend due to budget constraints, the coordination between agencies and siloed investments in digitization.
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   )
