0d464adbf4051983d27d1cb4e4ccb8110c123c05
Steve Conrad <sconrad@digitalimpactalliance.org>
2022-05-18 17:10:27 -0500

    Updates for build errors

--- pages/_sites/govstack/components/articles.js
+++ pages/_sites/govstack/components/articles.js
@@ -2,15 +2,15 @@ import React from 'react'
 import Card from './card'
 
 const Articles = ({ articles }) => {
-  const leftArticlesCount = Math.ceil(articles.length / 5)
-  const leftArticles = articles.slice(0, leftArticlesCount)
-  const rightArticles = articles.slice(leftArticlesCount, articles.length)
+  const leftArticlesCount = Math.ceil(articles?.length / 5)
+  const leftArticles = articles?.slice(0, leftArticlesCount)
+  const rightArticles = articles?.slice(leftArticlesCount, articles?.length)
 
   return (
     <div>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4' data-uk-grid='true'>
         <div>
-          {leftArticles.map((article, i) => {
+          {leftArticles?.map((article, i) => {
             return (
               <Card
                 article={article}
@@ -21,7 +21,7 @@ const Articles = ({ articles }) => {
         </div>
         <div>
           <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4' data-uk-grid>
-            {rightArticles.map((article, i) => {
+            {rightArticles?.map((article, i) => {
               return (
                 <Card
                   article={article}
--- pages/_sites/govstack/components/card.js
+++ pages/_sites/govstack/components/card.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
-import NextImage from './image'
+import NextImage from 'next/image'
+import { getStrapiMedia } from '../../../../lib/media'
 
 const Card = ({ article }) => {
 
   return (
     <div className='border-2 border-dial-gray hover:border-dial-yellow text-building-block hover:text-dial-yellow cursor-pointer'>
-      <Link href={`/article/${article.attributes.slug}`}>
+      <Link href={`/article/${article?.attributes.slug}`}>
         <a>
           <div className='p-3 uk-card uk-card-muted'>
             <div className='uk-card-media-top'>
-              <NextImage image={article.attributes.image} />
+              { article && (
+                <NextImage layout='responsive' objectFit='contain' width={article.attributes.image.data.attributes.width} height={article.attributes.image.data.attributes.height} src={getStrapiMedia(article.attributes.image)} alt={article.attributes.image.data.attributes.alternativeText || ''} />
+              )}
             </div>
             <div className='p-3'>
               <p id='category' className='uk-text-uppercase'>
-                {article.attributes.category?.data.attributes.name}
+                {article?.attributes.category?.data.attributes.name}
               </p>
               <p id='title' className='uk-text-large'>
-                {article.attributes.title}
+                {article?.attributes.title}
               </p>
             </div>
           </div>
