2a254990d540a91294b7db8d6b0368a0cb44fdc0
Steve Conrad <sconrad@digitalimpactalliance.org>
2022-09-06 16:48:59 -0500

    Updates to bb_fs page

--- pages/bb_fs/index.js
+++ pages/bb_fs/index.js
@@ -94,15 +94,15 @@ const BBFullScreen = () => {
             <div key={i} className='m-2'>
               <a href={`https://solutions.dial.community/building_blocks/${getSlug(bb)}`}>
                 <div className='h-full flex flex-col border border-dial-gray hover:border-dial-yellow card-drop-shadow'>
-                  <div className={`text-2xl text-white font-semibold overflow-hidden h-20 w-full flex flex-row gap-x-1.5 p-2 border-b border-dial-gray product-card-header ${bb_status ? bb_status : 'future'}`}>
+                  <div className={`text-2xl text-white font-semibold overflow-hidden h-1/5 w-full flex flex-row gap-x-1.5 p-2 border-b border-dial-gray product-card-header ${bb_status ? bb_status : 'future'}`}>
                     {bb.name}
                   </div>
-                  <div className='flex flex-col h-80 p-4'>
-                    <div className='m-auto w-1/2 h-1/2'>
+                  <div className='flex flex-col p-4 h-1/2'>
+                    <div className='mx-auto py-4 w-1/2'>
                       <img src={`https://solutions.dial.community/assets/building_blocks/${getSlug(bb)}.png`} alt='building block image' />
                     </div>
                   </div>
-                  <div className='border bg-dial-gray p-2 h-28'>
+                  <div className='border bg-dial-gray p-2 h-1/3 overflow-hidden'>
                     {bb.desc}
                   </div>
                 </div>
