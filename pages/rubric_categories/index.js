405ea0c5cb782251a90d21e6e20412f49bf6ea22
Michał Dulko <mdulko@soldevelo.com>
2022-09-08 10:06:48 +0000

    CDS-1045: Create rubric category list page

---
+++ pages/rubric_categories/index.js
@@ -0,0 +1,48 @@
+import { useCallback, useContext } from 'react'
+import { useSession } from 'next-auth/client'
+import { NextSeo } from 'next-seo'
+import { useIntl } from 'react-intl'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import SearchFilter from '../../components/shared/SearchFilter'
+import { UserFilterContext, UserFilterDispatchContext } from '../../components/context/UserFilterContext'
+import { Loading, Unauthorized } from '../../components/shared/FetchStatus'
+import ClientOnly from '../../lib/ClientOnly'
+import { useUser } from '../../lib/hooks'
+import RubricCategoryListQuery from '../../components/rubric-categories/RubricCategoryList'
+
+const RubricCategories = () => {
+  const { formatMessage } = useIntl()
+  const format = useCallback((id, values) => formatMessage({ id }, values), [formatMessage])
+
+  const [session] = useSession()
+  const { isAdminUser, loadingUserSession } = useUser(session)
+
+  const { search } = useContext(UserFilterContext)
+  const { setSearch } = useContext(UserFilterDispatchContext)
+
+  return (
+    <>
+      <NextSeo title={format('rubric-categories.label')}/>
+      <Header />
+      <ClientOnly>
+        {loadingUserSession ? <Loading /> : isAdminUser ? (
+          <>
+            <SearchFilter
+              search={search}
+              setSearch={setSearch}
+              hint='filter.entity.rubric-categories'
+              switchView={false}
+              exportJson={false}
+              exportCsv={false}
+            />
+            <RubricCategoryListQuery />
+          </>
+        ) : <Unauthorized />}
+      </ClientOnly>
+      <Footer />
+    </>
+  )
+}
+
+export default RubricCategories
