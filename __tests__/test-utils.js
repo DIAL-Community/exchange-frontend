22326cbd4a9a33af5f81bc775ee24e6f2c131b3f
Steve Conrad <sconrad@digitalimpactalliance.org>
2022-09-28 13:34:48 -0500

    Increase wait for flaky snapshot on test

--- __tests__/test-utils.js
+++ __tests__/test-utils.js
@@ -26,7 +26,7 @@ const waitForReactSelectToLoad = (container) => (
   waitFor(() => expect(container.querySelector('.react-select__loading-indicator')).toBeNull())
 )
 
-export const waitForAllEffects = async (waitTimeout = 100) => (
+export const waitForAllEffects = async (waitTimeout = 20) => (
   await act(() => new Promise((resolve) => setTimeout(resolve, waitTimeout)))
 )
 
