105385c629c7f97cb6934692bff8fc6abd0f942e
Steve Conrad <sconrad@digitalimpactalliance.org>
2023-07-18 09:00:49 -0500

    More updates for GDPIR

--- pages/_sites/gdpir/components/Landing.js
+++ pages/_sites/gdpir/components/Landing.js
@@ -3,9 +3,6 @@ import ProjectMap from '../../../../components/maps/projects/ProjectMap'
 const Landing = () => {
   return (
     <div className='relative overflow-hidden landing-with-menu'>
-      <div className='m-6 text-xl text-dial-gray-dark'>
-        Tracking the Development and Use of Digital Public Infrastructure around the world
-      </div>
       <ProjectMap />
     </div>
   )
--- pages/_sites/gdpir/index.js
+++ pages/_sites/gdpir/index.js
@@ -15,6 +15,13 @@ const HomePage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
+      <div className="max-w-catalog mx-auto image-block-hack image-h-full-hack relative">
+        <img className='w-screen h-3/4' height='500' src="images/gdpir/hero.png" alt='GDPIR Hero Image.' />
+        <img className='absolute left-20 bottom-40' width='500' height='500' src="images/gdpir/g20.png" alt='G20 Logo' />
+        <div className='absolute text-3xl font-bold text-dial-gray-dark top-20 left-20'>
+          Tracking the<br /><br />Development and Use<br /><br />of Digital Public Infrastructure<br /><br />around the world
+        </div>
+      </div>
       <Landing />
       <Footer />
     </>
