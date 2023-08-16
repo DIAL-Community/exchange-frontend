fb50eed078b15097887caa7d2966157f5270ca79
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-08-16 10:17:08 -0400

    Removing experimental feature from nextjs.

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ module.exports = withBundleAnalyzer(withRemoveImports({
     defaultLocale: 'en',
     localeDetection: false
   },
-  experimental: {
-    appDir: false
-  },
   images: {
     domains: [
       'localhost',
