508739c610227a5f65f676e9b3dd2d1f6de3cf49
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-09-05 12:30:48 -0400

    Open principle url in new tab.

--- ui/v1/principles/DigitalPrinciple.js
+++ ui/v1/principles/DigitalPrinciple.js
@@ -7,21 +7,27 @@ const DigitalPrinciple = ({ principle, index }) => {
 
   const displayLargeCard = () =>
     <div className={`px-4 py-3 rounded-lg ${index % 2 === 0 && 'bg-dial-violet'}`}>
-      <div className='flex flex-col lg:flex-row gap-x-6 gap-y-3'>
-        <div className='w-10 h-10 block'>
+      <div className='flex flex-row gap-x-6 gap-y-3'>
+        <div className='w-10 h-10 min-w-[2.5rem] block'>
           <img
             src={`/images/principles/${principle.slug}.png`}
             alt={format('ui.image.logoAlt', { name: format('ui.principle.label') })}
             className='object-contain w-10 h-10 mx-auto'
           />
         </div>
-        <div className='text-sm font-semibold text-dial-plum my-auto'>
-          {principle.name}
+        <div className='text-dial-plum my-auto border-b border-dial-iris-blue'>
+          <div className='text-sm font-semibold line-clamp-1'>
+            {principle.name}
+          </div>
         </div>
       </div>
     </div>
 
-  return displayLargeCard()
+  return (
+    <a href={principle.url} target='_blank' rel='noreferrer'>
+      {displayLargeCard()}
+    </a>
+  )
 }
 
 export default DigitalPrinciple
