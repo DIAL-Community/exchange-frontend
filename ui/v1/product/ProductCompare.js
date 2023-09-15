e5e2ac590ee9095170bead3aaa5275909e50e2eb
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-09-15 14:29:12 -0400

    Adding maturity data to the compare.

--- ui/v1/product/ProductCompare.js
+++ ui/v1/product/ProductCompare.js
@@ -5,6 +5,8 @@ import { useQuery } from '@apollo/client'
 import { PRODUCT_COMPARE_QUERY } from '../shared/query/product'
 import Breadcrumb from '../shared/Breadcrumb'
 import { Error, Loading, NotFound } from '../shared/FetchStatus'
+import BarChart from '../shared/BarChart'
+import RadarChart from '../shared/RadarChart'
 
 const ProductDetail = ({ slugs }) => {
   const { formatMessage } = useIntl()
@@ -32,7 +34,7 @@ const ProductDetail = ({ slugs }) => {
     return map
   })()
 
-  const compareFields = [
+  const fields = [
     'ui.sector.label',
     'product.license',
     'ui.buildingBlock.label',
@@ -42,7 +44,7 @@ const ProductDetail = ({ slugs }) => {
     'ui.sdg.label'
   ]
 
-  const renderFieldValue = (fieldValue) => {
+  const renderValueField = (fieldValue) => {
     if (Array.isArray(fieldValue)) {
       return (
         <div className='flex flex-col gap-y-2'>
@@ -58,35 +60,62 @@ const ProductDetail = ({ slugs }) => {
     return fieldValue
   }
 
+  const renderMaturityField = (maturityDetails) => {
+    const MAX_SCORE = 100
+    const MIN_RADAR_CATEGORIES = 2
+    const chartValues = () =>
+      maturityDetails?.map(({ overallScore, maximumScore }) => (overallScore / maximumScore) * MAX_SCORE)
+
+    const chartLabels = () => maturityDetails?.map(({ name }) => name)
+
+    return maturityDetails.length <= MIN_RADAR_CATEGORIES
+      ? <BarChart
+        labels={chartLabels()}
+        values={chartValues()}
+        maxScaleValue={MAX_SCORE}
+        fontSize={8}
+        horizontal
+      />
+      : <RadarChart
+        labels={chartLabels()}
+        values={chartValues()}
+        maxScaleValue={MAX_SCORE}
+        fontSize={8}
+      />
+  }
+
   return (
     <div className='lg:px-8 xl:px-56 flex flex-col'>
       <div className='px-4 lg:px-6 py-4 bg-dial-spearmint text-dial-stratos ribbon-detail z-40'>
         <Breadcrumb slugNameMapping={slugNameMapping}/>
       </div>
       <div className='flex flex-col'>
-        {compareFields.map((compareField, index) =>
-          <div key={index} className='flex flex-row'>
+        {fields.map((field, index) =>
+          <div key={index} className='flex flex-row text-sm text-dial-stratos'>
             <div
               className={classNames(
-                'basis-1/4 shrink-0',
+                `basis-1/${products.length + 1} grow-0	shrink-0`,
                 index % 2 === 0 && 'bg-dial-slate-300'
               )}
             >
               <div className='py-6 px-4'>
-                {format(compareField)}
+                {format(field)}
               </div>
             </div>
             {products.map((product, productIndex) =>
               <div
                 key={productIndex}
                 className={classNames(
-                  'basis-1/4 shrink-0',
+                  `basis-1/${products.length + 1} grow-0	shrink-0`,
                   'border-l border-dashed border-dial-slate-500',
                   index % 2 === 0 && 'bg-dial-slate-300'
                 )}
               >
-                <div className='py-6 px-4 text-sm text-dial-stratos'>
-                  {renderFieldValue(product[compareField])}
+                <div className='py-6 px-4'>
+                  {field === 'ui.product.rubric.label'
+                    ? renderMaturityField(product[field])
+                    : renderValueField(product[field])
+                  }
                 </div>
               </div>
             )}
