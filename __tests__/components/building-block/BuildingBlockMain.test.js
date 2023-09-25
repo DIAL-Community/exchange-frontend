c3ff5d3e5106bf072e9bd02840764c3898587290
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-09-25 18:31:54 -0400

    Building block main page test.

---
+++ __tests__/components/building-block/BuildingBlockMain.test.js
@@ -0,0 +1,44 @@
+import { screen } from '@testing-library/dom'
+import { render } from '../../test-utils'
+import { mockNextUseRouter } from '../../utils/nextMockImplementation'
+import BuildingBlockMain from '../../../components/building-block/BuildingBlockMain'
+import CustomMockedProvider, { generateMockApolloData } from '../../utils/CustomMockedProvider'
+import {
+  BUILDING_BLOCK_PAGINATION_ATTRIBUTES_QUERY,
+  PAGINATED_BUILDING_BLOCKS_QUERY
+} from '../../../components/shared/query/buildingBlock'
+import { QueryParamContextProvider } from '../../../components/context/QueryParamContext'
+import { BuildingBlockFilterProvider } from '../../../components/context/BuildingBlockFilterContext'
+import { paginatedBuildingBlocks, buildingBlockPaginationAttribute } from './data/BuildingBlockMain.data'
+
+mockNextUseRouter()
+describe('Unit tests for the building block main page.', () => {
+  test('Should render list of building blocks.', async () => {
+    const mockBuildingBlockPaginationAttribute = generateMockApolloData(
+      BUILDING_BLOCK_PAGINATION_ATTRIBUTES_QUERY,
+      { search: '', sdgs: [], useCases: [], workflows: [], categoryTypes: [], showMature:false },
+      null,
+      buildingBlockPaginationAttribute
+    )
+    const mockPaginatedBuildingBlocks = generateMockApolloData(
+      PAGINATED_BUILDING_BLOCKS_QUERY,
+      { search: '', sdgs: [], useCases: [], workflows: [], categoryTypes: [], showMature:false, limit: 8, offset: 0 },
+      null,
+      paginatedBuildingBlocks
+    )
+
+    render(
+      <CustomMockedProvider mocks={[mockPaginatedBuildingBlocks, mockBuildingBlockPaginationAttribute]}>
+        <QueryParamContextProvider>
+          <BuildingBlockFilterProvider>
+            <BuildingBlockMain activeTab={0} />
+          </BuildingBlockFilterProvider>
+        </QueryParamContextProvider>
+      </CustomMockedProvider>
+    )
+
+    expect(screen).toMatchSnapshot()
+    expect(await screen.findByText('Analytics and business intelligence')).toBeInTheDocument()
+    expect(await screen.findByText('Artificial intelligence')).toBeInTheDocument()
+  })
+})
