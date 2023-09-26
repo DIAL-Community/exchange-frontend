4def19ae1c25f2d17adc08aa82462213bfd82865
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-09-26 09:19:42 -0400

    Adding unit tests for use cases.

---
+++ __tests__/components/use-case/data/UseCaseMain.data.js
@@ -0,0 +1,95 @@
+export const paginatedUseCases = {
+  'data': {
+    'paginatedUseCases': [
+      {
+        'id': '61',
+        'slug': 'construction_permit',
+        'name': 'Construction Permit',
+        'imageFile': '/assets/use_cases/use_case_placeholder.svg',
+        'maturity': 'PUBLISHED',
+        'markdownUrl': 'https://raw.githubusercontent.com/inf-1-construction-permit.md',
+        'sector': {
+          'id': '390',
+          'slug': 'sustainable_infrastructure',
+          'name': 'Infrastructure durable'
+        },
+        'sanitizedDescription': 'Some description',
+        'buildingBlocks': null,
+        'sdgTargets': [
+          {
+            'id': '93',
+            'name': 'SDG Target 93'
+          },
+          {
+            'id': '96',
+            'name': 'SDG Target 96'
+          }
+        ]
+      },
+      {
+        'id': '59',
+        'slug': 'extended_producer_responsibility',
+        'name': 'Extended Producer Responsibility (EPR)',
+        'imageFile': '/assets/use_cases/use_case_placeholder.svg',
+        'maturity': 'PUBLISHED',
+        'markdownUrl': 'https://raw.githubusercontent.com/env-1-extended-producer-responsibility-epr.md',
+        'sector': {
+          'id': '852',
+          'slug': 'environment_and_climate',
+          'name': 'Environment and Climate'
+        },
+        'sanitizedDescription': 'Some other description',
+        'buildingBlocks': null,
+        'sdgTargets': [
+          {
+            'id': '96',
+            'name': 'SDG Target 96'
+          },
+          {
+            'id': '104',
+            'name': 'SDG Target 104'
+          },
+          {
+            'id': '105',
+            'name': 'SDG Target 105'
+          },
+          {
+            'id': '106',
+            'name': 'SDG Target 106'
+          }
+        ]
+      },
+      {
+        'id': '57',
+        'slug': 'agricultural_supply_chain_manage',
+        'name': 'Farmer-Facing Supply Chain Management',
+        'imageFile': '/assets/use_cases/use_case_placeholder.svg',
+        'maturity': 'PUBLISHED',
+        'markdownUrl': null,
+        'sector': {
+          'id': '853',
+          'slug': 'agriculture_and_rural_developmen',
+          'name': 'Agriculture and Rural Development',
+          '__typename': 'Sector'
+        },
+        'sanitizedDescription': 'Some more description',
+        'buildingBlocks': null,
+        'sdgTargets': [
+          {
+            'id': '103',
+            'name': 'SDG Target 103'
+          }
+        ]
+      }
+    ]
+  }
+}
+
+export const useCasePaginationAttribute = {
+  'data': {
+    'paginationAttributeUseCase': {
+      'totalCount': 10,
+      '__typename': 'PaginationAttributes'
+    }
+  }
+}
