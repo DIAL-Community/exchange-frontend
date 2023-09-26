b3d7bc6c5476fe2980556e28932d448926645f1b
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2023-09-26 15:21:36 -0400

    Adding dataset unit test.

---
+++ __tests__/components/dataset/data/DatasetMain.data.js
@@ -0,0 +1,185 @@
+export const paginatedDatasets = {
+  'data': {
+    'paginatedDatasets': [
+      {
+        'id': '159',
+        'name': 'Advocacy Training for Community Health Workers',
+        'slug': 'advocacy_training_for_community',
+        'imageFile': '/assets/datasets/content_placeholder.svg',
+        'tags': [],
+        'datasetDescription': {
+          'id': '6228',
+          'description': 'A free digital training that equips Community Health Workers (CHWs)',
+          'locale': 'en'
+        },
+        'sdgs': [
+          {
+            'id': '23'
+          },
+          {
+            'id': '21'
+          }
+        ],
+        'sectors': []
+      },
+      {
+        'id': '153',
+        'name': 'African Drone and Data Academy',
+        'slug': 'african_drone_and_data_academy',
+        'imageFile': '/assets/datasets/content_placeholder.svg',
+        'tags': [],
+        'datasetDescription': {
+          'id': '6219',
+          'description': 'MOOCs that provide youth with 21st-century skills related to drone technology and data analysis',
+          'locale': 'en'
+        },
+        'sdgs': [
+          {
+            'id': '28'
+          },
+          {
+            'id': '21'
+          }
+        ],
+        'sectors': []
+      },
+      {
+        'id': '38',
+        'name': 'African Storybook',
+        'slug': 'african_storybook',
+        'imageFile': '/assets/datasets/content_placeholder.svg',
+        'tags': [],
+        'datasetDescription': {
+          'id': '6217',
+          'description': 'Provides open access to picture storybooks.',
+          'locale': 'en'
+        },
+        'sdgs': [
+          {
+            'id': '28'
+          }
+        ],
+        'sectors': [
+          {
+            'id': '198'
+          }
+        ]
+      },
+      {
+        'id': '20',
+        'name': 'African Terrestrial Fibre Network Mapping Project',
+        'slug': 'african_terrestrial_fibre_networ',
+        'imageFile': '/assets/datasets/dataset_placeholder.svg',
+        'tags': [],
+        'datasetDescription': {
+          'id': '80',
+          'description': 'Open Source map of fibre optic infrastructure projects across the African continent.',
+          'locale': 'en'
+        },
+        'sdgs': [
+          {
+            'id': '24'
+          }
+        ],
+        'sectors': [
+          {
+            'id': '246'
+          }
+        ]
+      },
+      {
+        'id': '1',
+        'name': 'AfricaRISING Dataverse',
+        'slug': 'africarising_dataverse',
+        'imageFile': '/assets/products/africarising_dataverse.png',
+        'tags': [],
+        'datasetDescription': {
+          'id': '1655',
+          'description': 'Africa Research in Sustainable Intensification for the Next Generation (Africa RISING)',
+          'locale': 'en'
+        },
+        'sdgs': [
+          {
+            'id': '32'
+          },
+          {
+            'id': '32'
+          }
+        ],
+        'sectors': [
+          {
+            'id': '178'
+          }
+        ]
+      },
+      {
+        'id': '61',
+        'name': 'AI Agro',
+        'slug': 'ai_agro',
+        'imageFile': '/assets/products/ai_agro.png',
+        'tags': [],
+        'datasetDescription': {
+          'id': '6142',
+          'description': 'Open Source Precision Agriculture with Drones leverages a set of machine learning algorithms',
+          'locale': 'en'
+        },
+        'sdgs': [
+          {
+            'id': '32'
+          },
+          {
+            'id': '24'
+          },
+          {
+            'id': '30'
+          }
+        ],
+        'sectors': []
+      },
+      {
+        'id': '78',
+        'name': 'Angaza Elimu',
+        'slug': 'angaza_elimu',
+        'imageFile': '/assets/datasets/content_placeholder.svg',
+        'tags': [],
+        'datasetDescription': {
+          'id': '3085',
+          'description': 'Adaptive assessments for better learning outcomes',
+          'locale': 'en'
+        },
+        'sdgs': [
+          {
+            'id': '28'
+          }
+        ],
+        'sectors': []
+      },
+      {
+        'id': '160',
+        'name': 'Antura and the Letters',
+        'slug': 'antura_and_the_letters',
+        'imageFile': '/assets/products/antura_and_the_letters.png',
+        'tags': [],
+        'datasetDescription': {
+          'id': '6230',
+          'description': ' Open source smartphone application that teaches children how to read in Arabic.',
+          'locale': 'en'
+        },
+        'sdgs': [
+          {
+            'id': '28'
+          }
+        ],
+        'sectors': []
+      }
+    ]
+  }
+}
+
+export const datasetPaginationAttribute = {
+  'data': {
+    'paginationAttributeDataset': {
+      'totalCount': 165
+    }
+  }
+}
