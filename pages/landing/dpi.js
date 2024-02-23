5c9aa1780220b6e1409de6fb7e38455195c47854
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-02-23 09:00:06 +0800

    Fixing lint issue.

--- pages/landing/dpi.js
+++ pages/landing/dpi.js
@@ -1,9 +1,9 @@
-import { useIntl } from 'react-intl'
 import { NextSeo } from 'next-seo'
-import ClientOnly from '../../lib/ClientOnly'
-import Header from '../../components/shared/Header'
+import { useIntl } from 'react-intl'
 import Footer from '../../components/shared/Footer'
+import Header from '../../components/shared/Header'
 import QueryNotification from '../../components/shared/QueryNotification'
+import ClientOnly from '../../lib/ClientOnly'
 
 const DpiPage = () => {
   const { formatMessage } = useIntl()
@@ -22,9 +22,18 @@ const DpiPage = () => {
           <div className='xl:min-h-screen'>
             <div className="max-w-catalog mx-auto image-block-hack image-h-full-hack relative">
               <img className='w-screen h-3/4' height='500' src="images/gdpir/hero.png" alt='GDPIR Hero Image.' />
-              <img className='absolute left-20 bottom-40' width='500' height='500' src="images/gdpir/g20.png" alt='G20 Logo' />
+              <img
+                className='absolute left-20 bottom-40'
+                width='500'
+                height='500'
+                src="images/gdpir/g20.png"
+                alt='G20 Logo'
+              />
               <div className='absolute text-3xl font-bold text-dial-gray-dark top-20 left-20'>
-                Tracking the<br /><br />Development and Use<br /><br />of Digital Public Infrastructure<br /><br />around the world
+                Tracking the<br /><br />
+                Development and Use<br /><br />
+                of Digital Public Infrastructure<br /><br />
+                around the world
               </div>
             </div>
           </div>
