c6843af4afec16c7cbf91c209452c5c5b4805ebe
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-04-30 09:13:29 -0400

    Using parser instead of tiny mce.

--- components/shared/form/HtmlViewer.js
+++ components/shared/form/HtmlViewer.js
@@ -1,68 +1,12 @@
-import React, { useRef, useState } from 'react'
-import classNames from 'classnames'
+import React from 'react'
 import parse from 'html-react-parser'
-import { FaSpinner } from 'react-icons/fa6'
-import { Editor } from '@tinymce/tinymce-react'
 
-export const HtmlViewer = ({ initialContent, editorId, className, fontSize, lineHeight }) => {
-  const editorRef = useRef(null)
-  const [loading, setLoading] = useState(true)
+export const HtmlViewer = ({ initialContent }) => {
 
   return (
     <div className='relative'>
-      {loading && (
-        <FaSpinner size='2em' className='absolute text-lg inset-x-1/2 top-10 spinner' />
-      )}
       <div className='html-react-parser-viewer text-base'>
         {initialContent && parse(initialContent)}
-      </div>
-      <div className={classNames(className, 'html-viewer')}>
-        <Editor
-          id={editorId || 'tinymce-editor'}
-          apiKey={process.env.NEXT_PUBLIC_EDITOR_KEY}
-          onInit={(_, editor) => {
-            setLoading(false)
-            editorRef.current = editor
-          }}
-          value={initialContent}
-          init={{
-            min_height: 72,
-            selector: '#' + editorId ?? 'tinymce-editor',
-            menubar: false,
-            toolbar: false,
-            statusbar: false,
-            plugins: 'autoresize',
-            content_style: `
-              html {
-              }
-
-              body {
-                margin: 0;
-                font-family: 'Poppins', sans-serif;
-                /* Using text-base from tailwind */
-                font-size: ${fontSize ?? '1rem'};
-                line-height: ${lineHeight ?? '1.5rem'};
-                overflow: auto;
-              }
-
-              .mce-content-body[data-mce-placeholder]:not(.mce-visualblocks)::before {
-                margin-left: 1rem;
-                color: #dfdfea;
-              }
-
-              .mce-content-body > p:first-child {
-                margin: 0;
-              }
-
-              .mce-content-body > #stakeholders {
-                font-size: 1.125rem;
-                line-height: 1.75rem;
-              }
-            `,
-            branding: false
-          }}
-          disabled
-        />
       </div>
     </div>
   )
