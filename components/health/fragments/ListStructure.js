82557387263b72a802662246ec2716e7cd576b9a
Steve Conrad <sconrad@dial.global>
2024-06-28 09:42:16 -0500

    Initial work on pages for health tenant

---
+++ components/health/fragments/ListStructure.js
@@ -0,0 +1,61 @@
+import { useContext } from 'react'
+import { useQuery } from '@apollo/client'
+import { ProductFilterContext } from '../../context/ProductFilterContext'
+import { Error, Loading, NotFound } from '../../shared/FetchStatus'
+import { PAGINATED_PRODUCTS_QUERY } from '../../shared/query/product'
+import { DisplayType } from '../../utils/constants'
+import ProductCard from '../../product/ProductCard'
+
+const ListStructure = ({ pageOffset, defaultPageSize }) => {
+  const { search } = useContext(ProductFilterContext)
+
+  const { isLinkedWithDpi, showGovStackOnly, showDpgaOnly } = useContext(ProductFilterContext)
+  const { useCases, buildingBlocks, sectors, tags } = useContext(ProductFilterContext)
+  const { countries, licenseTypes, sdgs, origins, workflows } = useContext(ProductFilterContext)
+
+  const { loading, error, data } = useQuery(PAGINATED_PRODUCTS_QUERY, {
+    variables: {
+      search,
+      useCases: useCases.map(useCase => useCase.value),
+      buildingBlocks: buildingBlocks.map(buildingBlock => buildingBlock.value),
+      sectors: sectors.map(sector => sector.value),
+      tags: tags.map(tag => tag.label),
+      countries: countries.map(country => country.value),
+      licenseTypes: licenseTypes.map(licenseType => licenseType.value),
+      sdgs: sdgs.map(sdg => sdg.value),
+      workflows: workflows.map(workflow => workflow.id),
+      origins: origins.map(origin => origin.value),
+      isLinkedWithDpi,
+      showGovStackOnly,
+      showDpgaOnly,
+      limit: defaultPageSize,
+      offset: pageOffset
+    }
+  })
+
+  if (loading) {
+    return <Loading />
+  } else if (error) {
+    return <Error />
+  } else if (!data?.paginatedProducts) {
+    return <NotFound />
+  }
+
+  const { paginatedProducts: products } = data
+
+  return (
+    <div className='flex flex-col gap-3'>
+      {products.map((product, index) =>
+        <div key={index}>
+          <ProductCard
+            index={index}
+            product={product}
+            displayType={DisplayType.LARGE_CARD}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ListStructure
