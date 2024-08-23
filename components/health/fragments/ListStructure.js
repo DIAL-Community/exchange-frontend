0a3c77b21bfc3280cb2c795f1584aa7b777c2073
mdulko <mdulko@soldevelo.com>
2024-08-23 02:48:17 +0200

    CDS-1942: Create product list page and rectangular product card

--- components/health/fragments/ListStructure.js
+++ components/health/fragments/ListStructure.js
@@ -4,7 +4,8 @@ import { ProductFilterContext } from '../../context/ProductFilterContext'
 import { Error, Loading, NotFound } from '../../shared/FetchStatus'
 import { PAGINATED_PRODUCTS_QUERY } from '../../shared/query/product'
 import { DisplayType } from '../../utils/constants'
-import ProductCard from '../../product/ProductCard'
+import ProductCard from '../../health/fragments/ProductCard'
+import ProductFilter from './ProductFilter'
 
 const ListStructure = ({ pageOffset, defaultPageSize }) => {
   const { search } = useContext(ProductFilterContext)
@@ -43,16 +44,13 @@ const ListStructure = ({ pageOffset, defaultPageSize }) => {
   const { paginatedProducts: products } = data
 
   return (
-    <div className='flex flex-col gap-3'>
-      {products.map((product, index) =>
-        <div key={index}>
-          <ProductCard
-            index={index}
-            product={product}
-            displayType={DisplayType.LARGE_CARD}
-          />
-        </div>
-      )}
+    <div className='px-4 lg:px-8 min-h-[70vh] py-8'>
+      <ProductFilter />
+      <div className='grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-8'>
+        {products.map((product, index) =>
+          <ProductCard key={index} product={product} displayType={DisplayType.GRID_CARD} urlPrefix='/health' />
+        )}
+      </div>
     </div>
   )
 }
