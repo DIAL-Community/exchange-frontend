b3654645dee928bb2a7b320955871afa35d47d26
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-08-22 20:53:58 -0400

    Adding resources, fao and health tenant urls to the image domain list.

--- next.config.js
+++ next.config.js
@@ -15,7 +15,16 @@ module.exports = withBundleAnalyzer(withRemoveImports({
       'solutions-dev.dial.community',
       'solutions.dial.community',
       'exchange.dial.global',
-      'exchange-dev.dial.global'
+      'exchange-dev.dial.global',
+      // Dev DPI
+      'dpi.dial.community',
+      // Production DPI
+      'dpi.dial.global',
+      'resource.dial.global',
+      // Health related images
+      'health.dial.community',
+      // FAO related images
+      'fao.dial.community'
     ]
   }
 }))
