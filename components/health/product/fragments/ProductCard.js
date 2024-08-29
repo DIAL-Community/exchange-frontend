12ee4d6dd42d9daf55424f513bb12b4db25e0ec1
mdulko <mdulko@soldevelo.com>
2024-08-29 11:16:00 +0200

    CDS-1942: adjust styles in product list, fixed lineClamps

--- components/health/product/fragments/ProductCard.js
+++ components/health/product/fragments/ProductCard.js
@@ -1,4 +1,4 @@
-import { useCallback, useContext } from 'react'
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import { FaXmark } from 'react-icons/fa6'
 import { useIntl } from 'react-intl'
@@ -14,6 +14,9 @@ const ProductCard = ({ displayType, product, dismissHandler, urlPrefix = null })
   const { comparedProducts } = useContext(ProductFilterContext)
   const { setComparedProducts } = useContext(ProductFilterDispatchContext)
 
+  const textRef = useRef(null)
+  const [lineClamp, setLineClamp] = useState(0)
+
   const isInComparedProducts = () => {
     return comparedProducts.filter(p => p.id === product.id).length > 0
   }
@@ -26,57 +29,81 @@ const ProductCard = ({ displayType, product, dismissHandler, urlPrefix = null })
     }
   }
 
+  useEffect(() => {
+    const adjustLineClamp = () => {
+      if (textRef.current) {
+        const element = textRef.current
+        const lineHeight = parseInt(window.getComputedStyle(element).lineHeight, 10)
+        let height = element.offsetHeight - 10
+
+        if (height > 79) {
+          height = 79
+        }
+
+        if (height < 79 && height > 48) {
+          height = 60
+        }
+
+        const lines = Math.floor(height / lineHeight)
+
+        setLineClamp(lines)
+      }
+    }
+
+    adjustLineClamp()
+  }, [])
+
   const displayGridCard = () =>
     <div className='cursor-pointer hover:rounded-lg hover:shadow-lg border-3 border-transparent hover:border-dial-sunshine'>
       <div
-        className='bg-white shadow-lg rounded-lg h-full border border-dial-gray hover:border-transparent'>
-        <div className="flex flex-col">
-
-          <div className="flex text-dial-sapphire bg-dial-alice-blue h-20 rounded-t-lg">
-            <div className="px-4 text-sm text-center font-semibold m-auto">
-              {product.name}
-            </div>
+        className='bg-white shadow-lg rounded-xl h-360 border border-dial-gray hover:border-transparent'>
+        <div className="flex flex-col h-full">
+          <div className="flex justify-center items-center py-12 bg-white rounded-xl border-health-red border-4 mx-4 my-4">
+            {product.imageFile.indexOf('placeholder.svg') < 0 &&
+              <div className="inline">
+                <img
+                  src={process.env.NEXT_PUBLIC_GRAPHQL_SERVER + product.imageFile}
+                  alt={format('ui.image.logoAlt', { name: format('ui.product.label') })}
+                  className="object-contain"
+                />
+              </div>
+            }
+            {product.imageFile.indexOf('placeholder.svg') >= 0 &&
+              <div className="w-20 h-20">
+                <img
+                  src={process.env.NEXT_PUBLIC_GRAPHQL_SERVER + product.imageFile}
+                  alt={format('ui.image.logoAlt', { name: format('ui.product.label') })}
+                  className="object-contain dial-meadow-filter"
+                />
+              </div>
+            }
           </div>
-          <div className="mx-auto py-6">
-            <img
-              className="object-contain h-20 w-20"
-              layout="fill"
-              alt={format('image.alt.logoFor', { name: product.name })}
-              src={process.env.NEXT_PUBLIC_GRAPHQL_SERVER + product.imageFile}
-            />
+          <div className="px-6 text-xl text-center font-semibold m-auto text-health-blue">
+            {product.name}
           </div>
-          <hr/>
-          <div className="text-xs text-dial-stratos font-semibold">
-            <div className="px-4 py-2 flex gap-2">
-              <span className="my-auto text-truncate-3">
+          <div className="text-xs text-dial-stratos font-medium h-full overflow-hidden " ref={textRef}>
+            <div className="px-6 py-2 flex  mx-auto gap-2">
+              <span
+                className="my-auto text-center m-auto dynamic-truncate"
+                style={{
+                  WebkitLineClamp: lineClamp
+                }}
+              >
                 {product.parsedDescription}
               </span>
             </div>
           </div>
-          <hr/>
-          <div className="text-xs text-dial-stratos font-semibold uppercase">
-            <div className="px-4 py-2 flex gap-2">
-              <span className="badge-avatar w-7 h-7">
-                {product.softwareCategories?.length}
-              </span>
-              <span className="my-auto">
-                {product.softwareCategories?.length
-                  ? product.softwareCategories[0].name
-                  : format('ui.category.header')
-                }
-              </span>
-            </div>
-          </div>
-          <hr/>
-          <div className="text-xs text-dial-stratos font-semibold uppercase">
-            <div className="px-4 py-3 flex gap-2">
-              <span className="my-auto">
-                {product.commercialProduct
-                  ? format('product.pricing.commercial').toUpperCase()
-                  : product.mainRepository?.license?.toUpperCase() || format('general.na')
-                }
-              </span>
-            </div>
+          <div className="text-xs text-dial-stratos font-semibold">
+            {
+              product?.softwareCategories[0]?.name &&
+                <div className="px-4 flex gap-2 align-end">
+                  <span className="mb-3 mx-auto">
+                    <div className="rounded-full bg-health-red uppercase shadow-none px-6 py-1 text-white text-xs">
+                      <div className="line-clamp-1">{product?.softwareCategories[0]?.name}</div>
+                    </div>
+                  </span>
+                </div>
+            }
           </div>
         </div>
       </div>
