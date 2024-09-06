ab0e973e4d360d7810b1f622f4ea4a2149be92b8
mdulko <mdulko@soldevelo.com>
2024-09-06 12:17:14 +0200

    CDS-2026: changed useStates to useForm while editing section

--- components/health/product/fragments/ProductDetailExtraAttributes.js
+++ components/health/product/fragments/ProductDetailExtraAttributes.js
@@ -2,50 +2,38 @@ import { useMutation } from '@apollo/client'
 import { useRouter } from 'next/router'
 import React, { useCallback, useContext, useState, useEffect } from 'react'
 import { useIntl } from 'react-intl'
+import { Controller, useForm } from 'react-hook-form'
 import { useUser } from '../../../../lib/hooks'
 import { ToastContext } from '../../../../lib/ToastContext'
 import EditableSection from '../../../shared/EditableSection'
 import { UPDATE_PRODUCT_EXTRA_ATTRIBUTES } from '../../../shared/mutation/product'
 import Input from '../../../shared/form/Input'
+import { ProductExtraAttributeNames } from '../../../utils/constants'
 
 const ProductDetailExtraAttributes = ({ product, canEdit, headerRef }) => {
   const { formatMessage } = useIntl()
   const format = useCallback((id, values) => formatMessage({ id }, values), [formatMessage])
 
-  const [extraAttributes, setExtraAttributes] = useState(product.extraAttributes)
   const [isDirty, setIsDirty] = useState(false)
+  const [initialValues, setInitialValues] = useState([])
 
-  const [ownership, setOwnership] = useState('')
-  const [ownershipType, setOwnershipType] = useState('')
-  const [funders, setFunders] = useState('')
-  const [fundersType, setFundersType] = useState('')
-  const [impact, setImpact] = useState('')
-  const [impactType, setImpactType] = useState('')
-  const [years, setYears] = useState('')
-  const [yearsType, setYearsType] = useState('')
+  const { control, handleSubmit, reset: VariablesReset } = useForm({
+    defaultValues: {
+      extraAttributes: ProductExtraAttributeNames.map(name => ({ name, value: '', type: '' }))
+    }
+  })
 
   useEffect(() => {
-    product.extraAttributes.forEach(attr => {
-      switch (attr.name) {
-      case 'Local Ownership':
-        setOwnership(attr.value)
-        setOwnershipType(attr.type)
-        break
-      case 'Funders':
-        setFunders(attr.value)
-        setFundersType(attr.type)
-        break
-      case 'Impact':
-        setImpact(attr.value)
-        setImpactType(attr.type)
-        break
-      case 'Years in production':
-        setYears(attr.value)
-        setYearsType(attr.type)
-        break
-      }
-    })
-  }, [product.extraAttributes])
+    if (product?.extraAttributes.length) {
+      const formattedExtraAttributes = ProductExtraAttributeNames.map(name => {
+        const existingAttr = product.extraAttributes.find(attr => attr.name === name)
+
+        return existingAttr || { name, value: '', type: '' }
+      })
+      setInitialValues(formattedExtraAttributes)
+      VariablesReset({ extraAttributes: formattedExtraAttributes })
+    }
+  }, [VariablesReset])
 
   const { user } = useUser()
   const { locale } = useRouter()
@@ -55,7 +43,6 @@ const ProductDetailExtraAttributes = ({ product, canEdit, headerRef }) => {
   const [updateProductExtraAttributes, { loading, reset }] = useMutation(UPDATE_PRODUCT_EXTRA_ATTRIBUTES, {
     onError() {
       setIsDirty(false)
-      setExtraAttributes(product?.extraAttributes)
       showFailureMessage(format('toast.submit.failure', { entity: format('ui.extraAttributes.header') }))
       reset()
     },
@@ -63,46 +50,24 @@ const ProductDetailExtraAttributes = ({ product, canEdit, headerRef }) => {
       const { updateProductExtraAttributes: response } = data
       if (response?.product && response?.errors?.length === 0) {
         setIsDirty(false)
-        setExtraAttributes(response?.product?.extraAttributes)
         showSuccessMessage(format('toast.submit.success', { entity: format('ui.extraAttributes.header') }))
       } else {
         setIsDirty(false)
-        setExtraAttributes(product?.extraAttributes)
         showFailureMessage(format('toast.submit.failure', { entity: format('ui.extraAttributes.header') }))
         reset()
       }
     }
   })
 
-  const onSubmit = () => {
+  const onSubmit = (data) => {
     if (user) {
       const { userEmail, userToken } = user
+      const { extraAttributes } = data
 
       updateProductExtraAttributes({
         variables: {
           slug: product?.slug,
-          extraAttributes: [
-            {
-              name: 'Local Ownership',
-              value: ownership,
-              type: ownershipType
-            },
-            {
-              name: 'Funders',
-              value: funders,
-              type: fundersType
-            },
-            {
-              name: 'Impact',
-              value: impact,
-              type: impactType
-            },
-            {
-              name: 'Years in production',
-              value: years,
-              type: yearsType
-            }
-          ]
+          extraAttributes
         },
         context: {
           headers: {
@@ -115,7 +80,7 @@ const ProductDetailExtraAttributes = ({ product, canEdit, headerRef }) => {
   }
 
   const onCancel = () => {
-    setExtraAttributes(product.extraAttributes)
+    VariablesReset({ extraAttributes: initialValues })
     setIsDirty(false)
   }
 
@@ -124,9 +89,9 @@ const ProductDetailExtraAttributes = ({ product, canEdit, headerRef }) => {
       {format('ui.extraAttributes.label')}
     </div>
 
-  const displayModeBody = extraAttributes.length
+  const displayModeBody = product?.extraAttributes.length
     ? <div className='flex flex-col gap-y-2'>
-      {extraAttributes?.map((attribute, index) =>
+      {product?.extraAttributes?.map((attribute, index) =>
         <div key={`extraAttribute-${index}`}>
           <div key={index} className='text-black'>
             {`${attribute.name}: ${attribute.value.length ? attribute.value : format('general.na')}`}
@@ -141,118 +106,51 @@ const ProductDetailExtraAttributes = ({ product, canEdit, headerRef }) => {
   const editModeBody =
     <>
       <div className='px-4 lg:px-6 py-4 flex flex-col gap-y-3 text-sm'>
-        <div className='grid grid-cols-4 gap-y-2 gap-x-4'>
-          <label className='col-span-3'>
-            {format('extraAttributes.ownership')}
-          </label>
-          <label className='col-span-1'>
-            {format('extraAttributes.type')}
-          </label>
-          <Input
-            className='col-span-4 md:col-span-3'
-            id="ownership"
-            value={ownership}
-            onChange={(e) => {
-              setOwnership(e.target.value)
-              setIsDirty(true)
-            }}
-            placeholder={format('extraAttributes.ownership')}
-          />
-          <Input
-            className='col-span-4 md:col-span-1'
-            id="ownershipType"
-            value={ownershipType}
-            onChange={(e) => {
-              setOwnershipType(e.target.value)
-              setIsDirty(true)
-            }}
-            placeholder={format('extraAttributes.type')}
-          />
-        </div>
-        <div className="grid grid-cols-4 gap-y-2 gap-x-4">
-          <label className="col-span-3">
-            {format('extraAttributes.funders')}
-          </label>
-          <label className="col-span-1">
-            {format('extraAttributes.type')}
-          </label>
-          <Input
-            className='col-span-4 md:col-span-3'
-            id="funders"
-            value={funders}
-            onChange={(e) => {
-              setFunders(e.target.value)
-              setIsDirty(true)
-            }}
-            placeholder={format('extraAttributes.funders')}
-          />
-          <Input
-            className='col-span-4 md:col-span-1'
-            id="fundersType"
-            value={fundersType}
-            onChange={(e) => {
-              setFundersType(e.target.value)
-              setIsDirty(true)
-            }}
-            placeholder={format('extraAttributes.type')}
-          />
-        </div>
-        <div className="grid grid-cols-4 gap-y-2 gap-x-4">
-          <label className="col-span-3">
-            {format('extraAttributes.impact')}
-          </label>
-          <label className="col-span-1">
-            {format('extraAttributes.type')}
-          </label>
-          <Input
-            className='col-span-4 md:col-span-3'
-            id="impact"
-            value={impact}
-            onChange={(e) => {
-              setImpact(e.target.value)
-              setIsDirty(true)
-            }}
-            placeholder={format('extraAttributes.impact')}
-          />
-          <Input
-            className='col-span-4 md:col-span-1'
-            id="impactType"
-            value={impactType}
-            onChange={(e) => {
-              setImpactType(e.target.value)
-              setIsDirty(true)
-            }}
-            placeholder={format('extraAttributes.type')}
-          />
-        </div>
-        <div className="grid grid-cols-4 gap-y-2 gap-x-4">
-          <label className="col-span-3">
-            {format('extraAttributes.years')}
-          </label>
-          <label className="col-span-1">
-            {format('extraAttributes.type')}
-          </label>
-          <Input
-            className='col-span-4 md:col-span-3'
-            id="years"
-            value={years}
-            onChange={(e) => {
-              setYears(e.target.value)
-              setIsDirty(true)
-            }}
-            placeholder={format('extraAttributes.years')}
-          />
-          <Input
-            className='col-span-4 md:col-span-1'
-            id="yearsType"
-            value={yearsType}
-            onChange={(e) => {
-              setYearsType(e.target.value)
-              setIsDirty(true)
-            }}
-            placeholder={format('extraAttributes.type')}
-          />
-        </div>
+        <form>
+          {ProductExtraAttributeNames.map((name, index) => (
+            <div key={name} className="grid grid-cols-4 gap-2 mt-2">
+              <label className="col-span-3">{name}</label>
+              <label className="col-span-1">Type</label>
+              <Controller
+                name={`extraAttributes.${index}.value`}
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    className="col-span-3"
+                    placeholder={name}
+                    onChange={(e) => {
+                      field.onChange(e)
+                      setIsDirty(true)
+                    }}
+                  />
+                )}
+              />
+              <Controller
+                name={`extraAttributes.${index}.type`}
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    className="col-span-1"
+                    placeholder={format('extraAttributes.type')}
+                    onChange={(e) => {
+                      field.onChange(e)
+                      setIsDirty(true)
+                    }}
+                  />
+                )}
+              />
+              <Controller
+                name={`extraAttributes.${index}.name`}
+                control={control}
+                render={({ field }) => (
+                  <input type="hidden" {...field} value={name} />
+                )}
+              />
+            </div>
+          ))}
+        </form>
       </div>
     </>
 
@@ -260,7 +158,7 @@ const ProductDetailExtraAttributes = ({ product, canEdit, headerRef }) => {
     <EditableSection
       canEdit={canEdit}
       sectionHeader={sectionHeader}
-      onSubmit={onSubmit}
+      onSubmit={handleSubmit(onSubmit)}
       onCancel={onCancel}
       isDirty={isDirty}
       isMutating={loading}
