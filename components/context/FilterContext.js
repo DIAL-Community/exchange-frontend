a7c26891d82355270052821b22c1bd9aa3aa8985
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-09-13 11:53:58 -0400

    Updating default filter context.

--- components/context/FilterContext.js
+++ components/context/FilterContext.js
@@ -1,37 +1,30 @@
 import { createContext, useState } from 'react'
 
-export const convertToKey = (s) => s.replace(/\s+/g, '_').toLowerCase()
-export const FILTER_ITEMS = [ 'filter.entity.organizations' ]
-
 const FilterContext = createContext()
-const initialCounts = (() => {
-  return FILTER_ITEMS.reduce((map, item) => {
-    map[item] = item === 'filter.entity.maps' ? '3' : '0'
-
-    return map
-  }, {})
-})()
+const FilterDispatchContext = createContext()
 
 const FilterContextProvider = ({ children }) => {
   const [search, setSearch] = useState('')
-  const [resultCounts, setResultCounts] = useState(initialCounts)
   // Task tracker context only
   const [showFailedOnly, setShowFailedOnly] = useState(false)
 
-  const props = {
+  const valueProps = {
     search,
-    resultCounts,
-    showFailedOnly,
+    showFailedOnly
+  }
+
+  const dispatchProps = {
     setSearch,
-    setResultCounts,
     setShowFailedOnly
   }
 
   return (
-    <FilterContext.Provider value={{ ...props }}>
-      {children}
+    <FilterContext.Provider value={{ ...valueProps }}>
+      <FilterDispatchContext.Provider value={{...dispatchProps}}>
+        {children}
+      </FilterDispatchContext.Provider>
     </FilterContext.Provider>
   )
 }
 
-export { FilterContextProvider, FilterContext }
+export { FilterContextProvider, FilterContext, FilterDispatchContext }
