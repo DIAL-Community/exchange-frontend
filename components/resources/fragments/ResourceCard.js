f8407ced5ecbcaf95f9b7b08bb7273021b803989
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-09-19 15:51:45 -0400

    Aligning logo in the middle.

--- components/resources/fragments/ResourceCard.js
+++ components/resources/fragments/ResourceCard.js
@@ -73,7 +73,7 @@ const ResourceCard = ({ displayType, index, resource, dismissHandler }) => {
               <img
                 src={process.env.NEXT_PUBLIC_GRAPHQL_SERVER + resource.imageFile}
                 alt={format('ui.image.logoAlt', { name: format('ui.resource.label') })}
-                className='aspect-auto h-[267px]'
+                className='mx-auto aspect-auto h-[267px]'
               />
             }
           </Link>
