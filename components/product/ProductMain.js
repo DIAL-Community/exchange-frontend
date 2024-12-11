42c842b5f81424cc1e7f8747cbbcb718b307271b
Nyoman Ribeka <nyoman.ribeka@gmail.com>
2024-12-11 17:28:12 -0500

    Grid view for products.

--- components/product/ProductMain.js
+++ components/product/ProductMain.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
+import { CollectionDisplayType, FilterContext } from '../context/FilterContext'
 import ProductMainLeft from './ProductMainLeft'
 import ProductMainRight from './ProductMainRight'
 
 const ProductMain = ({ activeTab }) => {
-  return (
+  const { collectionDisplayType } = useContext(FilterContext)
+
+  const listDisplay = (
     <div className='px-4 lg:px-8 xl:px-56'>
       <div className='grid grid-cols-3 gap-x-8'>
         <div className='hidden md:block col-span-1'>
@@ -14,6 +18,14 @@ const ProductMain = ({ activeTab }) => {
       </div>
     </div>
   )
+
+  const gridDisplay = (
+    <div className='px-4 lg:px-8 xl:px-56'>
+      <ProductMainRight activeTab={activeTab} />
+    </div>
+  )
+
+  return collectionDisplayType === CollectionDisplayType.LIST ? listDisplay : gridDisplay
 }
 
 export default ProductMain
