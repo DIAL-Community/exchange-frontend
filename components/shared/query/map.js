a9b58283848beb4770a5a9453362fe4f1ba7ecd4
Steve Conrad <sconrad@dial.global>
2025-02-28 17:28:49 -0600

    Put map queries for healthtech marketplace back in

--- components/shared/query/map.js
+++ components/shared/query/map.js
@@ -65,6 +65,51 @@ export const ORGANIZATIONS_QUERY = gql`
   }
 `
 
+export const PRODUCTS_QUERY = gql`
+  query SearchProducts(
+    $first: Int,
+    $countries: [String!]
+    $products: [String!]
+    ) {
+    searchProducts(
+      first: $first,
+      countries: $countries,
+      products: $products
+    ) {
+      totalCount
+      pageInfo {
+        endCursor
+        startCursor
+        hasPreviousPage
+        hasNextPage
+      }
+      nodes {
+        id
+        name
+        slug
+        countries {
+          id
+          name
+          slug
+          code
+        }
+      }
+    }
+  }
+`
+
+export const COUNTRIES_QUERY = gql`
+  query Countries($search: String) {
+    countries(search: $search) {
+      id
+      name
+      slug
+      latitude
+      longitude
+    }
+  }
+`
+
 export const AGGREGATORS_MAP_QUERY = gql`
   query SearchOrganizations(
     $aggregatorOnly: Boolean
